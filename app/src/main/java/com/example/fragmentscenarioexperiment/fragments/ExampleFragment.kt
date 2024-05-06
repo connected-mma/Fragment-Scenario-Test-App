ebc1cbd1bdbd930f1972cb886052882555d8b5b4
Matthew Ma <mma@connected.io>
2024-05-06 14:49:16 -0400

    Change to using DaggerViewHostFragment

--- app/src/main/java/com/example/fragmentscenarioexperiment/fragments/ExampleFragment.kt
+++ app/src/main/java/com/example/fragmentscenarioexperiment/fragments/ExampleFragment.kt
@@ -2,19 +2,23 @@ package com.example.fragmentscenarioexperiment.fragments
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
 import com.example.fragmentscenarioexperiment.R
+import com.example.fragmentscenarioexperiment.di.support.DaggerViewHostFragment
 import com.example.fragmentscenarioexperiment.utils.ExampleFragmentDelegate
-import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
-class ExampleFragment : DaggerFragment(R.layout.fragment_example) {
+class ExampleFragment : DaggerViewHostFragment() {
 
     @Inject lateinit var delegate: ExampleFragmentDelegate
 
     private lateinit var titleText: TextView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.fragment_example, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
